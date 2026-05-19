b98eff0bf52a26e4d7a93725d214cb7cf4eae852
Robinson <153916131+RobinsonDeveloper@users.noreply.github.com>
2026-05-20 01:02:41 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,9 +1,7 @@
-# App.java
-cat > src/main/java/com/example/App.java << 'EOF'
 package com.example;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins Maven Demo!");
     }
 }
-EOF
